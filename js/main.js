d729e8cfbc2df5a277d25bd9c3f4f800f33bb7e3
Angel Ruiz <aruizca@gmail.com>
2015-04-18 15:19:50 +1000

    Publish of Github pages from Gradle.

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function() {
+    if (typeof hljs != 'undefined') {
+        hljs.initHighlightingOnLoad();
+    }
+
+    if ($().bootstrapNews) {
+        $("#news").bootstrapNews({
+            newsPerPage: 2,
+            autoplay: true,
+            pauseOnHover: true,
+            navigation: true,
+            direction: 'up',
+            newsTickerInterval: 6000,
+            onToDo: function () {
+                console.log(this);
+            }
+        });
+    }
+
+    if ($().magnificPopup) {
+        $('.content a.image').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: false,
+            fixedContentPos: true,
+            mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+            image: {
+                verticalFit: true
+            },
+            zoom: {
+                enabled: true,
+                duration: 300 // don't foget to change the duration also in CSS
+            }
+        });
+    }
+
+    if ($().tocify) {
+        //Calls the tocify method on your HTML div.
+        $("#toc").tocify({
+            context: '#documentation',
+            selectors: 'h2,h3',
+            scrollTo: 80,
+            extendPage: false
+        });
+    }
+
+    if ($().iFrameResize) {
+        $('#examples-iframe').iFrameResize({
+            checkOrigin: false
+        });
+
+        $("#examples-iframe").on("load",function() {
+            alert(1);
+            console.log($('#examples-iframe').contents());
+        });
+    }
+});
